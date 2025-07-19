c807084f2271a1b008e76388dc8ce88b833e1eba
John Benton <jbenton4494@gmail.com>
2025-07-19 13:07:26 -0400

    plus/minus ops

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -11,9 +11,9 @@ type env = {
 }
 
 let codegen_op ~env:env token =
-  let _i8_type = Llvm.i8_type env.the_context in
+  let i8_type = Llvm.i8_type env.the_context in
   let i32_type = Llvm.i32_type env.the_context in
-  let _i8_array_type = Llvm.array_type _i8_type 30000 in
+  let i8_array_type = Llvm.array_type i8_type 30000 in
   match token with
   | T_INC ->
     let load_ptr = Llvm.build_load i32_type env.ptr "load_ptr" env.builder in
@@ -27,7 +27,25 @@ let codegen_op ~env:env token =
     let dec_ptr = Llvm.build_sub load_ptr const_one "dec_ptr" env.builder in
     let _ = Llvm.build_store dec_ptr env.ptr env.builder in
     ()
-  | op -> failwith ("Op " ^ (token_to_string op) ^ " not supported")
+  | T_PLUS ->
+    let load_ptr = Llvm.build_load i32_type env.ptr "load_ptr" env.builder in
+    let tape_cell_addr = Llvm.build_gep i8_array_type env.tape 
+      [|Llvm.const_int i32_type 0; load_ptr|] "tape_cell_addr" env.builder in
+    let tape_cell_value = Llvm.build_load i8_type tape_cell_addr "tape_cell_val" env.builder in
+    let const_one = Llvm.const_int i8_type 1 in
+    let add_to_cell = Llvm.build_add tape_cell_value const_one "add_to_cell" env.builder in
+    let _ = Llvm.build_store add_to_cell tape_cell_addr env.builder in 
+    ()
+  | T_MINUS ->
+    let load_ptr = Llvm.build_load i32_type env.ptr "load_ptr" env.builder in
+    let tape_cell_addr = Llvm.build_gep i8_array_type env.tape 
+      [|Llvm.const_int i32_type 0; load_ptr|] "tape_cell_addr" env.builder in
+    let tape_cell_value = Llvm.build_load i8_type tape_cell_addr "tape_cell_val" env.builder in
+    let const_one = Llvm.const_int i8_type 1 in
+    let sub_to_cell = Llvm.build_sub tape_cell_value const_one "sub_to_cell" env.builder in
+    let _ = Llvm.build_store sub_to_cell tape_cell_addr env.builder in 
+    ()
+  | op -> failwith ("Op " ^ (token_to_string op) ^ " not supported yet")
 
 let generate tokens = 
   let the_context = Llvm.create_context () in
--- test/test_bfvm.ml
+++ test/test_bfvm.ml
@@ -3,10 +3,10 @@ open Bfvm.Parser
 
 let list_to_str f tokens = String.concat "--" (List.map f tokens)
 let test_simple () = 
-  let tokens = parse "inputs/simple.bf" in
+  let tokens = parse "inputs/ops.bf" in
   let tokens_str = list_to_str token_to_string tokens in
   let expected = "T_PLUS--T_MINUS--T_DEC--T_INC--T_DOT--" ^
-                    "T_COMMA--T_LBRAC--T_RBRAC--T_EOF" in
+                    "T_COMMA--T_LBRAC--T_RBRAC" in
   check string "Not same strings" expected tokens_str
 
 let suite =
